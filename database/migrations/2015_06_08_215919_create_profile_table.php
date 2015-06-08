133dcb5a9bd92d1f7aa1dea0f0b2c430ab86278f
Eric Debelak <ericdebelak@gmail.com>
2015-06-08 17:47:15 -0600

    profile migration

---
+++ database/migrations/2015_06_08_215919_create_profile_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfileTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('profiles', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('bio');
+			$table->integer('user_id')->unsigned();
+			$table->foreign('user_id')->references('id')->on('users');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('profiles');
+	}
+
+}
